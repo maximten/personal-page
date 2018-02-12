26ce236a74634780079f0d934ceac6767f75197e
Maxim Ten <maximten94@gmail.com>
2018-02-12 16:31:27 +0700

    animation update

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,53 @@
 import anime from 'animejs'
 
-const middleFrameAnimation = anime({
-  targets: document.getElementById("middle-frame"),
-  translateY: '100px',
+const initialAnimationProps = {
   rotateX: '60deg',
   rotateY: '0',
   rotateZ: '45deg',
   autoplay: false
+};
+
+const reactAnimation = anime({
+  targets: document.getElementById("react"),
+  translateX: '100px',
+  translateY: '100px',
+  ...initialAnimationProps,
 });
 
-const bottomFrameAnimation = anime({
-  targets: document.getElementById("bottom-frame"),
+const reduxAnimation = anime({
+  targets: document.getElementById("redux"),
+  translateX: '-100px',
+  translateY: '100px',
+  ...initialAnimationProps,
+});
+
+const nodejsAnimation = anime({
+  targets: document.getElementById("nodejs"),
+  translateX: '-100px',
   translateY: '200px',
-  rotateX: '60deg',
-  rotateY: '0',
-  rotateZ: '45deg',
-  autoplay: false
+  ...initialAnimationProps,
+});
+
+const laravelAnimation = anime({
+  targets: document.getElementById("laravel"),
+  translateX: '100px',
+  translateY: '200px',
+  ...initialAnimationProps,
+});
+
+const dockerAnimation = anime({
+  targets: document.getElementById("docker"),
+  translateY: '300px',
+  ...initialAnimationProps,
 });
 
 window.addEventListener('scroll', () => {
   const offset = document.documentElement.scrollTop || document.body.scrollTop;
   const animationProgress = Math.min(100 / (200 / offset), 100);
-  middleFrameAnimation.seek(animationProgress);
-  bottomFrameAnimation.seek(animationProgress);
+  reactAnimation.seek(animationProgress);
+  reduxAnimation.seek(animationProgress);
+  nodejsAnimation.seek(animationProgress);
+  laravelAnimation.seek(animationProgress);
+  dockerAnimation.seek(animationProgress);
 });
 
